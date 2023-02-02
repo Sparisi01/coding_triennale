a2ccf68425a6797dd3026fc19fef67f2cf7d5462
Sparisi01 <sparisi2002@gmail.com>
2023-02-02 18:38:53 +0100

    rename

--- Filtri_Immagini/sketch.js
+++ Filtri_Immagini/sketch.js
@@ -1,8 +1,21 @@
 
+var f_matrix;
+var image;
 
 function setup() {
   var canvas = createCanvas(640, 640);
   canvas.parent('canvasP5');
+}
+
+function filter(image, matrix){
+
+}
+
+function onPressedApply(){
+
+}
+
+function onPressedReset(){
 
 }
 
